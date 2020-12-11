bab6b55f073bbc9c47aa4297a243f6d508901218
KiNG-MBA <kingmba.6663@gmail.com>
2020-12-11 18:42:09 +0330

    Implementation MasterStudent class!!

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,15 +1,28 @@
 package com.company;
 
+import com.company.dao.MasterImpl;
 import com.company.dao.StudentImpl;
+import com.company.person.Master;
+import com.company.person.Person;
 import com.company.person.Student;
 
 public class Main {
 
     public static void main(String[] args) {
-        Student student = new Student("mehdi","alavai");
-        StudentImpl studentImpl = new StudentImpl();
 
-        studentImpl.insertRecord(student);
+        StudentImpl studentImpl = new StudentImpl();
+        MasterImpl masterImpl = new MasterImpl();
+
+        studentImpl.insertRecord(new Person("mahdi","ahmadi"));
+        masterImpl.insertRecord(new Master("Behnam","salili"));
+
+        studentImpl.updateRecord(new Person("masoud","hassannia"),2);
+
+
+
+
+
+
 
     }
 }
---
+++ src/main/java/com/company/dao/MasterStudentImpl.java
@@ -0,0 +1,59 @@
+package com.company.dao;
+
+import com.company.MasterStudent;
+import com.company.person.Person;
+
+import java.sql.*;
+
+public class MasterStudentImpl {
+    private final String address = "jdbc:mysql://localhost/university";
+    private final String user = "root";
+    private final String password = "0135790";
+
+    public void createTable() {
+        try (Connection connection = DriverManager.getConnection(address, user, password);
+             Statement statement = connection.createStatement();) {
+            String student = "Create Table IF NOT EXISTS master_student (" +
+                    "id int primary key auto_increment," +
+                    "master_ID int NOT NULL," +
+                    "student_ID int NOT NULL)";
+
+            statement.executeUpdate(student);
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+
+    public void insertRecord(MasterStudent masterStudent) {
+        try (Connection connection = DriverManager.getConnection(address, user, password);
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO master_student (master_ID,student_ID) VALUES(?,?)");) {
+            preparedStatement.setInt(1, masterStudent.getMasterID());
+            preparedStatement.setInt(1, masterStudent.getStudentID());
+            preparedStatement.executeUpdate();
+            System.out.println("New MasterStudent Added!");
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+    }
+
+    public void deleteRecord(int id) {
+        try (Connection connection = DriverManager.getConnection(address, user, password);
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "DELETE FROM master_student WHERE id = ?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("MasterStudent with ID " + id + " deleted!");
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+    }
+
+
+
+}
